47c8b814caa8399994669c4b1f5a6681d7dd5728
Miriam Mchome <mirimchome@gmail.com>
2017-10-06 22:39:00 +0200

    working db connection

--- app/src/main/java/android/app/printerapp/SLMData.java
+++ app/src/main/java/android/app/printerapp/SLMData.java
@@ -14,7 +14,14 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.common.collect.ImmutableMap;
+import com.strongloop.android.loopback.Model;
+import com.strongloop.android.loopback.ModelRepository;
+import com.strongloop.android.loopback.RestAdapter;
+import com.strongloop.android.loopback.callbacks.ObjectCallback;
+
 public class SLMData extends ActionBarActivity {
+
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -32,11 +39,54 @@ public class SLMData extends ActionBarActivity {
      */
     private ViewPager mViewPager;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.slmdata);
+       /*
+        !!!!!
+        How to query
 
+        RestAdapter adapter = new RestAdapter(getApplicationContext(), "http://api.lizardhead.xyz:3000/api");
+        ModelRepository partRepository = adapter.createRepository("Part");
+
+        partRepository.findById("part2", new ObjectCallback<Model>() {
+            @Override
+            public void onSuccess(Model part) {
+                // found!
+                System.out.println("##### query find worked :) !!!!");
+                System.out.println("##### the project for the part was.. data should have fifi in it  "+ part.toMap().values());
+            }
+
+            public void onError(Throwable t) {
+                // handle the error
+                System.out.println("##### mmhhh db had this issue with find "+ t.getMessage());
+            }
+        });
+
+        */
+
+        /*
+        !!!!!!
+        HOW TO DO SAVE
+        Model part = partRepository.createObject( ImmutableMap.of("PartId", "part3", "ProjectId","fifi", "StlFileName","part3.stl", "CadFileName","part3.cad") );
+
+        part.save(new Model.Callback() {
+            @Override
+            public void onSuccess() {
+                // saved!
+                System.out.println("##### saving worked dude check the db :) !!!!");
+            }
+            @Override
+            public void onError(Throwable t) {
+                // handle errors
+                System.out.println("##### mmhhh db had this issue  "+ t.getMessage());
+            }
+        });
+
+       */
         TextView parts = (TextView) findViewById(R.id.mark);
         parts.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,6 +106,52 @@ public class SLMData extends ActionBarActivity {
             }
         });
     }
+
+    
+    public class Part extends Model{
+
+        private String PartId;
+        private String ProjectId;
+        private String StlFileName;
+        private String CadFileName;
+
+        public String getStlFileName() {
+            return StlFileName;
+        }
+
+        public void setStlFileName(String stlFileName) {
+            StlFileName = stlFileName;
+        }
+
+        public String getCadFileName() {
+            return CadFileName;
+        }
+
+        public void setCadFileName(String cadFileName) {
+            CadFileName = cadFileName;
+        }
+
+        public String getPartId() {
+            return PartId;
+        }
+
+        public void setPartId(String partId) {
+            PartId = partId;
+        }
+
+        public String getProjectId() {
+            return ProjectId;
+        }
+
+        public void setProjectId(String projectId) {
+            ProjectId = projectId;
+        }
+
+
+
+
+    }
+
 
 
 }
